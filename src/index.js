cf8ba9930f5343a8a4d629c5b2ad4e19703c9282
Alec <amsears60441@gmail.com>
2024-01-01 17:40:29 -0700

    made background in fixed position

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 50;
 
-const renderer = new THREE.WebGLRenderer();
+// const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('myCanvas')});
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // OrbitControls setup
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 
